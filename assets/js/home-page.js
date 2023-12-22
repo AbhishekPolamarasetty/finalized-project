1108ff0ec39ebd7a4ede1bcddc96ebe0e065407d
AbhishekPolamarasetty <pashek321@gmail.com>
2023-12-22 17:16:56 +0530

    finalized home page but need to check responsive

---
+++ assets/js/home-page.js
@@ -0,0 +1,25 @@
+const goTopBtn = document.querySelector('.go-top-btn');
+
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight(){
+  if(window.scrollY > 200) {
+    goTopBtn.style.display = "flex"
+  } else {
+    goTopBtn.style.display = "none"
+  }
+}
+
+goTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+})
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
